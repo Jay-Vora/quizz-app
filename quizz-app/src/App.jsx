ea1d0a071ca510431e8c943e6c283511bd2bebef
vora1900 <vora1900@mylaurier.ca>
2025-05-16 16:18:08 -0600

    added few other files

--- quizz-app/src/App.jsx
+++ quizz-app/src/App.jsx
@@ -1,28 +1,5 @@
-import React, { useState } from 'react';
-import QuizStart from './components/QuizStart';
-import QuizQuestion from './components/QuizQuestion';
-import QuizResult from './components/QuizResult';
-import { questions } from './data/questions';
+const App = () => {
+  return <h1>Hello from Quiz App</h1>;
+};
 
-export default function App() {
-  const [step, setStep] = useState('start');
-  const [score, setScore] = useState(0);
-
-  const handleStart = () => setStep('quiz');
-  const handleFinish = (finalScore) => {
-    setScore(finalScore);
-    setStep('result');
-  };
-  const handleRetry = () => {
-    setScore(0);
-    setStep('start');
-  };
-
-  return (
-    <>
-      {step === 'start' && <QuizStart onStart={handleStart} />}
-      {step === 'quiz' && <QuizQuestion onFinish={handleFinish} />}
-      {step === 'result' && <QuizResult score={score} onRetry={handleRetry} />}
-    </>
-  );
-}
+export default App;
